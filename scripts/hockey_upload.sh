fd93cd6d0c1438be1134231e849c2ea8d1f7f776
pepibumur <pepibumur@gmail.com>
2014-03-04 21:44:11 +0100

    Changes

--- scripts/hockey_upload.sh
+++ scripts/hockey_upload.sh
@@ -3,11 +3,10 @@
 # Use in Wercker to upload builds automatically to HockeyApp
 # Checks environment variable UPLOAD_TO_HOCKEYAPP=1, so you can have multiple tests on the "development" branch but only upload once.
 # Expects an environment variable called HOCKEYAPP_TOKEN which is the API token
-cd build
-cd apk
-ls
 if [[ "$UPLOAD_TO_HOCKEYAPP" == "1" ]]; then
       echo "Uploading to HockeyApp, could take a while..."
+      ls build/
+      ls build/apk
       curl \
           -F "status=$HA_STATUS_AVAILABLE_FOR_DOWNLOAD" \
           -F "notify=$HA_NOTIFY_NONE" \
